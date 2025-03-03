112092377035b383e97a0b7f2c8b52fc10ce0954
WekesaKelvin <106185953+WekesaKelvin@users.noreply.github.com>
2025-03-03 13:45:23 +0300

    Update product-api.service.ts

--- src/app/services/product-api.service.ts
+++ src/app/services/product-api.service.ts
@@ -1,4 +1,4 @@
-// src/app/services/product-api.service.ts
+
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Product } from '../product.model';
