aad9243f16fbc7c0440500328022fefd81dbfa98
WekesaKelvin <106185953+WekesaKelvin@users.noreply.github.com>
2025-03-03 13:45:00 +0300

    Update product-list.component.ts

--- src/app/pages/product-list/product-list.component.ts
+++ src/app/pages/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-// src/app/pages/product-list/product-list.component.ts
+
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
